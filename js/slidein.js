70447fb436849b53ad9860f9a230c8e7f24f4d1c
AZUMAYUI <t13027ya@sfc.keio.ac.jp>
2014-05-01 15:28:09 +0900

    コメントを追加

--- js/slidein.js
+++ js/slidein.js
@@ -1,7 +1,12 @@
+//クリック時に呼ばれるアニメーションの関数
 var slideinFoxkeh = function(){
+	//変数に画像を代入
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
+	//画像にクラスを追加（=>アニメーションする）
 	foxkeh.setAttribute("class", "slidein");
 };
 
+//変数にボタンを代入
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
+//ボタンにクリックイベントを追加
 slideinFoxkehButton.addEventListener("click", slideinFoxkeh);
